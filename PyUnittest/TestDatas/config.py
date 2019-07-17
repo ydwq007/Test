e5646a3b4fc93cdd968c7dfda35480dd8bd06a50
ydwq007 <775636762@qq.com>
2019-07-17 10:06:24 +0800

    cap+移动端接口

---
+++ PyUnittest/TestDatas/config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+接口：移动端登录接口- unittest
+创建人：魏奇
+更新人：魏奇
+更新时间：2019-07-17
+"""
+#信息头
+headers = {
+    "Content-Type": "application/json"
+    #     # "Connection": "keep-alive"
+    # "Accept-Encoding": "gzip, deflate, br",
+    # "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"
+}
+
+#数据库
